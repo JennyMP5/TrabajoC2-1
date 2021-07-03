c8a0b4eafd2a334aeda0b6435b2f6c9c3ad4d493
JennyMP5 <86113827+JennyMP5@users.noreply.github.com>
2021-07-03 17:54:17 -0500

    Update colaborador2.2.h
    Primero que nada, un cordial saludo de mi parte soy Jenniffer Katherine Mina Preciado de tecnología de la información y comunicación (TIC) paralelo “A” y le hago la entrega de mi trabajo.
    Como nos podemos dar cuenta en el código ponemos primeramente 3 punteros (matriz1, matriz2 y resultado) para que almacene la dirección de memoria en esas variables. Que quiere decir los punteros van a almacenar tanto los valores que los usuarios le asignen y también la dirección de memoria donde se guarden.
    Los dos primeros for que logras ver a continuación es de la matriz 1 y matriz2 lo cual lo estamos ocupando para presentar por pantalla las dos matrices. Así mismo estamos dando inicio a la variable la cual nos va a guardar el resultado.
    Ya hecho esto se puede ver las matrices que ingresadas anteriormente para así poder hacer el proceso del producto (multiplicación).
    Por último, hacemos lo que es el producto que consiste básicamente en multiplicar las filas por las columnas (i=fila) (j=columnas) y el resultado de esta se va a presentar en otra matriz la cual la llamamos resultado=r.
    Gracia por su tiempo

--- colaborador2.2.h
+++ colaborador2.2.h
@@ -1,5 +1,36 @@
 void producto(float (*m1)[5], float (*m2)[5], float (*r)[5])
 {
+cout<<"\n Obtener el producto de las dos matrices ";
 
+cout<<"\nLa matriz 1 es:\n";
+for(int i=0;i<5;i++){
+	for(int j=0; j<5;j++){
+		cout<<m1[i][j]<<" ";
+	}
+	cout<<"\n";
+}
+cout<<"\nLa matriz 2 es:\n";
+for(int i=0;i<5;i++){
+        for(int j=0; j<5;j++){
+                cout<<m2[i][j]<<" ";
+        }
+        cout<<"\n";
+}
+
+cout<<"El producto de las dos matrices es";
+
+//Inicializamos la matriz c correspondiente al resultado del producto
+
+for(int i=0;i<5;++i)
+for(int j=0;j<5;++j)
+r[i][j]=0;
+
+//Generamos la matriz C que sria en estecso el resultado=r
+
+
+for(int i=0;i<5;++i)
+for(int j=0;j<5;++j)
+for(int z=0; z<5;++z)
+r[i][j]+=m1[i][z] * m2[z][j];
 cout<<"Obetener el producto de las dos matrices ";
 }
