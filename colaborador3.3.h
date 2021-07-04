595126e23a94e611b3c8aae3dcc0d144a58cd2c0
ArelY2002 <86209266+ArelY2002@users.noreply.github.com>
2021-07-03 21:13:12 -0500

    ZAMBRANO ZAMBRANO ARELY
    Antes  que nada, un cordial saludo de mi parte ..soy la alumna Zambrano Zambrano Arely  de tecnología de la información y comunicación (TIC) paralelo “A”  Calcular la pontencia "p" se crea un bucle para hacer la resta y mediante el bucle sacamos la potencia..

--- colaborador3.3.h
+++ colaborador3.3.h
@@ -1,5 +1,33 @@
+//zambrano zambrano arely
 void potencia(float (*m1)[5], int p, float (*r)[5])
+
 {
 
 cout<<"Calcular la potencia p de una ";
+  cout<<"\nLa matriz 1 es:\n";
+for(int i=0;i<5;i++){
+        for(int j=0; j<5;j++){
+                cout<<m1[i][j]<<" ";
+        }
+        cout<<"\n";
+}
+p=2;
+
+cout<<"La potencia "<<p<<"de la primera matriz es";
+
+//Inicializamos la matriz p correspondiente al resultado de la potencia
+
+for(int i=0;i<5;++i)
+for(int j=0;j<5;++j)
+r[i][j]=0;
+
+
+//Generamos la matriz p
+
+
+for(int i=0;i<5;++i)
+for(int j=0;j<5;++j)
+for(int z=0; z<5;++z)
+r[i][j]+=m1[i][z] * m1[z][j];
+
 }
