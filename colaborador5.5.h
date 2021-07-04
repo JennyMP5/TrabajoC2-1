e2f830e0dbf9cb706af2047aaa5c0b7cb4a728cf
Rosa Antonella Posso Becerra <86168170+rosaposso12@users.noreply.github.com>
2021-07-03 20:08:54 -0500

    Update colaborador5.5.h
    Esta función me va a permitir sacar la media y varianza de las dos matrices y compararlas para saber cuál es la mayor.
    
    1. 
    
    **Declaración de variables**
    Primero se declaran las variables que se van a utilizar como punteros para poder guardar espacios y acceder a sus direcciones, se utiliza void debido a que este archivo no va a retornar nada. dentro de los paréntesis de void declaró las variables que me van a servir para sacar la media y la varianza de cada matriz( se las declara como float porque se pueden ingresar números con decimales) y la variable n que representa a el número de elementos que en este caso es 25 porque son matrices de 5x5 (está variable se la declara como int porque el 25 es un número entero) 
    
    2.
    
    **Mostrar matrices**
    Primero se muestran las matrices utilizando dos ciclos for para poder presentar la por pantalla mientras se recorre cada elemento de la matriz en orden fila×columna.
    
    3.
    
    Sacar la media de las matrices
    Para sacar la media de las matrices primero usé dos bucles for (uno dentro del otro para que vayan ordenados fila×columna) que me permitirán ir realizando el proceso con cada elemento de la matriz; la fórmula de la media es **media=(X1+X2+X3+xn)/n** y el código que utilicé para calcular la matriz en c++ es este **media1+=(m1[i][j])/n;** que es equivalente a este **media1=media1+(m1[i][j])/n;**, y aquí establezco que la media de la matriz va a ser igual a la **media** (inicializada en 0) más cada elemento de la matriz y dividida para el número de elementos **n**; en otras palabras el bucle me permite sumar todos los elementos de la matriz y dividirlos para el número de elementos. 
    
    4.
    
    Sacar la varianza de las matrices
    Para sacar la varianza de las matrices también utilicé dos bucles for (uno dentro del otro para que vayan ordenados fila×columna) que me permiten ir realizando el proceso con cada elemento de la matriz; la fórmula de la varianza es **varianza=[(x1-media)²+(X2-media)²+(xn-media)²]/n** y el código que utilicé para calcular la varianza de las matrices en c++ es este **varianza2+=(m2[i][j]-media2)*(m2[i][j]-media2)/n;** que es equivalente a este **varianza2=varianza2+(m2[i][j]-media2)*(m2[i][j]-media2)/n;**; en este proceso se establezco que cada número de la matriz lo voy a restar con la media de la respectiva matriz **(m2[i][j]-media2)** y lo multiplicó por la misma **(m2[i][j]-media2)*(m2[i][j]-media2)** , todo este proceso se repite hasta que se sumen todos los resultados de cada elemento de la matriz y por último se lo divida para el número de elementos **n**.
    
    5.
    
    Comparación de las medias y las varianzas
    Para hacer la comparación entre las medias usé la estructura condicional **if-else** que me permitirá establecer que sí (**if( )**) una condición es verdadera realice ciertas instrucciones (*{  }**) y si no (**else{ }**) realice otras instrucciones. La condición que coloqué fue: **if(media1>media2){ court<<media1;}** que estable la condición en la que la media1 es mayor que la media2 y está condición es verdadera se presente por pantalla la media1 y si la condición es falsa se presente por pantalla la media2 **else{cout<<media2;}**.
    El mismo procedimiento realice para la comparación entre la varianza1 y varianza2.

--- colaborador5.5.h
+++ colaborador5.5.h
@@ -1,7 +1,179 @@
 void mediayvarianza(float (*m1)[5], float (*m2)[])
 {
 
-cout<<"Determinar  cual de las 2 matrices tiene la mayor media y varianza";
+cout<<"\nDeterminar  cual de las 2 matrices tiene la mayor media y varianza\n";
+
+
+//declaro las variables que voy a usar
+
+        float media1=0, media2=0, varianza1=0, varianza2=0;
+
+        int n;
+
+n=25;
+
+/*n: representa al número de elementos, en este caso son 25 elementos porque es una matriz de 5×5*/
+
+//la variable n me servira para la formula de media y varianza
+
+                                                                                                                                                                               
+  
+
+//Presento por pantalla la matriz1
+
+cout<<"\nLa matriz 1 es:\n";
+
+for(int i=0;i<5;i++){
+
+        for(int j=0; j<5;j++){
+
+                cout<<m1[i][j]<<" ";
+
+        }
+
+        cout<<"\n";
+
+}
+  
+
+/*Calculo la media de la primera matriz recorriendo la matriz con un bucle for para que se sumen todos sus elementos y despues se dividan para el numero de elementos n*/
+
+for (int i=0;i<5;i++){
+
+        for(int j=0;j<5;j++){
+
+                media1+=(m1[i][j])/n;
+
+        }
+
+}
+
+cout<<"\nLa media de la matriz 1 es : \n"<<media1;
+  
+
+/*Calculo la varianza de la primera matriz con un bucle for que me permite restar cada elemento con la media, despues se multiplique por si misma y por ultimo se divida para el
+
+numero de elementos n*/
+
+for (int i=0;i<5;i++){
+
+        for(int j=0;j<5;j++){
+
+                varianza1+=(m1[i][j]-media1)*(m1[i][j]-media1)/n;
+
+                }
+
+}
+
+cout<<"\nLa varianza de la matriz 1 es: "<<varianza1;
+
+cout<<endl;
+  
+  
+
+//Presento por pantalla la matriz2
+
+cout<<"\nLa matriz 2 es:\n";
+
+for(int i=0;i<5;i++){
+
+        for(int j=0; j<5;j++){
+
+                cout<<m2[i][j]<<" ";
+
+        }
+
+        cout<<"\n";
+
+}
+
+  
+/*Calculo la media de la segunda matriz recorriendo la matriz con un bucle for para que se sumen todos sus elementos y despues se dividan para el numero de elementos n*/
+
+for (int i=0;i<5;i++){
+
+        for(int j=0;j<5;j++){
+
+                media2+=(m2[i][j])/n;
+
+        }
+
+}
+
+cout<<"\nLa media de la matriz 2 es : \n"<<media2;
+
+cout<<endl;
+  
+
+/*Calculo la varianza de la segunda matriz con un bucle for que me permite restar cada elemento con la media, se multiplique por si misma, se sumen y por ultimo se divida para el
+
+numero de elementos n*/
+
+for (int i=0;i<5;i++){
+
+        for(int j=0;j<5;j++){
+
+                varianza2+=(m2[i][j]-media2)*(m2[i][j]-media2)/n;
+
+                }
+
+}
+
+cout<<"\nLa varianza de la matriz 2 es: "<<varianza2;
+
+cout<<endl;
+  
+
+/*A continuacion se compara la media de la matriz1 y la matriz2 con una estrucctura condicional if-else que me permita saber cual es mayor*/
+
+cout<<"____________________________________________________________________\n";
+
+if(media1>media2){
+
+        cout<<"\n//  La matriz 1 tiene la mayor media: "<<media1<<"  //";
+
+        cout<<endl;
+
+}
+
+else{
+
+        cout<<"\n//La matriz 2 tiene la mayor media: "<<media2<<"  //";;
+
+        cout<<endl;
+
+}
+
+cout<<"____________________________________________________________________\n";
+
+cout<<endl<<endl;
+
+  
+/*A continuacion se compara la varianza de la matriz1 y la matriz2 con una estrucctura condicional if-else que me permita saber cual es mayor*/
+
+cout<<"____________________________________________________________________\n";
+
+                                                                                                                                                                                
+  if(varianza1>varianza2){
+
+        cout<<"\n//  La matriz 1 tiene la mayor varianza: "<<varianza1<<"  //";
+
+        cout<<endl;
+
+}
+
+else{
+
+        cout<<"\n//  La matriz 2 tiene la mayor varianza: "<<varianza2<<"  //";
+
+        cout<<endl;
+
+}
+
+cout<<"____________________________________________________________________\n";
+
+}
+  
 
 
 
